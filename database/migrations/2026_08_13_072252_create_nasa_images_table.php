ce3d805fe1b96c9037f82753b0a0b3900da8594d
Maxim Belyi <btf9458@gmail.com>
2026-08-13 13:43:37 +0500

    feat(migrations): nasa migration add

---
+++ database/migrations/2026_08_13_072252_create_nasa_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nasa_images', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('nasa_topic_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('nasa_id')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image_url');
+            $table->dateTime('date_created');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nasa_images');
+    }
+};
